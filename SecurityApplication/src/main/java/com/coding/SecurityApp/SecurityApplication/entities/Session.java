4cac4ec80f11bd9ed1b281a9438f099f28a5def5
bsiddh <bsiddh667@gmail.com>
2026-07-05 13:15:28 +0530

    git auth ref added

--- SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/entities/Session.java
+++ SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/entities/Session.java
@@ -1,4 +1,31 @@
 package com.coding.SecurityApp.SecurityApplication.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String refreshToken;
+
+    @CreationTimestamp
+    private LocalDateTime lastUsedAt;
+
+    @ManyToOne
+    private User user;
+
+
+
 }
--- SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/services/SessionService.java
+++ SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/services/SessionService.java
@@ -1,4 +1,45 @@
 package com.coding.SecurityApp.SecurityApplication.services;
 
+import com.coding.SecurityApp.SecurityApplication.entities.Session;
+import com.coding.SecurityApp.SecurityApplication.entities.User;
+import com.coding.SecurityApp.SecurityApplication.repositories.SessionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SessionService {
+
+    private final SessionRepository sessionRepository;
+    private final int SESSION_LIMIT = 2;
+
+   public void generateNewSession(User user,String refreshToken){
+       List<Session> userSession = sessionRepository.findByUser(user);
+       if(userSession.size() == SESSION_LIMIT){
+           userSession.sort(Comparator.comparing(Session::getLastUsedAt));
+
+           Session leastReacentlyUsedSession = userSession.getFirst();
+           sessionRepository.delete(leastReacentlyUsedSession);
+       }
+
+       Session newSession = Session.builder()
+               .user(user)
+               .refreshToken(refreshToken)
+               .build();
+
+       sessionRepository.save(newSession);
+   }
+
+   public void validateSession(String refreshToken){
+     Session session =   sessionRepository.findByRefreshToken(refreshToken)
+             .orElseThrow(()->new SessionAuthenticationException("Session not found for refreshToken: "));
+       session.setLastUsedAt(LocalDateTime.now());
+       sessionRepository.save(session);
+   }
+
 }
