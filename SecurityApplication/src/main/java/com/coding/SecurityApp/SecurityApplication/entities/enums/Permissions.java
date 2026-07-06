fbb68cdbd67be9a7f2dbd665a6f8116b4cbea815
bsiddh <bsiddh667@gmail.com>
2026-07-06 11:03:41 +0530

    granular authority feature added

---
+++ SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/entities/enums/Permissions.java
@@ -0,0 +1,4 @@
+package com.coding.SecurityApp.SecurityApplication.entities.enums;
+
+public class Permissions {
+}
---
+++ SecurityApplication/src/main/java/com/coding/SecurityApp/SecurityApplication/utils/PermissionMapping.java
@@ -0,0 +1,4 @@
+package com.coding.SecurityApp.SecurityApplication.utils;
+
+public class PermissionMapping {
+}
